a461e3807439303f46091820e246b6c82e3ad7c7
deviana97 <mateusviana97@gmail.com>
2025-07-12 20:47:30 -0300

    Add user registration functionality with validation and password hashing

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,10 @@
 'use server';
 
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
-
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma"
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
   try {
     const user = signInFormSchema.parse({
@@ -25,4 +26,34 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 
 export async function signOutUser() {
   await signOut();
+}
+
+export async function signUpWithCredentials(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword')
+    });
+
+    const hashedPassword = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: hashedPassword
+      }
+    });
+
+    await signIn('credentials', { email: user.email, password: user.password });
+
+    return { success: true, message: 'User registered successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+    return { success: false, message: 'Failed to sign up' }
+  }
 }
