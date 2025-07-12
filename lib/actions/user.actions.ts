1a31940cb47acb5b2e730a97d02d3851ad56dc3a
deviana97 <mateusviana97@gmail.com>
2025-07-12 11:36:24 -0300

    Fix typo in signInFormSchema and update import in user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,12 @@
 'use server';
 
-import { singInFormSchema } from "../validators";
+import { signInFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 export async function singInWithCredentials(prevState: unknown, formData: FormData) {
   try {
-    const user = singInFormSchema.parse({
+    const user = signInFormSchema.parse({
       email: formData.get('email'),
       password: formData.get('password')
     })
--- lib/validators.ts
+++ lib/validators.ts
@@ -20,7 +20,7 @@ export const insertProductSchema = z.object({
   price: currency,
 })
 
-export const singInFormSchema = z.object({
+export const signInFormSchema = z.object({
   email: z.string().email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 })
